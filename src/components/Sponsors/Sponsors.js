95839736ef2cd5d00391c3e187cff394ca7f594b
SidtheKid0504 <63013935+SidtheKid0504@users.noreply.github.com>
2024-03-08 08:54:27 -0500

    Add files via upload

--- src/components/Sponsors/Sponsors.js
+++ src/components/Sponsors/Sponsors.js
@@ -1,17 +1,26 @@
 import {React, Component} from "react";
+import google from "./assets/google.png";
+import microsoft from "./assets/microsoft.png";
+import mongodb from "./assets/mongodb.png";
+import princeton from "./assets/princeton.png";
+import mpd from "./assets/mpd.png";
+import tigerstale from "./assets/tigerstale.png";
 import "./Sponsors.scss";
-
-
 
 export class Sponsors extends Component {
     render() {
         return (
             <div class="container">
                 <div class="sponsors-header">
-                    <h1>Sponsors</h1>
+                    <h1>Past Sponsors</h1>
                 </div>
                 <div class="sponsor-content">
-                    <h1>Sponsors will be revealed soon... 🤫</h1>
+                    <img id="sponsor-image" src={google} alt="Google"/>
+                    <img id="sponsor-image" src={microsoft} alt="Microsoft"/>
+                    <img id="sponsor-image" src={mongodb} alt="MongoDB"/>
+                    <img id="sponsor-image" src={princeton} alt="Princeton University"/>
+                    <img id="sponsor-image" src={mpd} alt="Montgomery Pediatric Dentistry"/>
+                    <img id="sponsor-image" src={tigerstale} alt="Tiger's Tale"/>
                 </div>
             </div>
         );
